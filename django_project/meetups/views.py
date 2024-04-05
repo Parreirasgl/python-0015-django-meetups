b94b1913b922dada1abf35e0d7a5e9d137d1d624
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-05 02:41:14 -0300

    added header and main on the html

--- django_project/meetups/views.py
+++ django_project/meetups/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
-def index(request): 
-    lista = [1, 2, 3, 4]
-    return render(request, 'meetups/index.html', {'var1': lista})
-
-
+def index(request):
+    meetups = [
+        {
+        'title': 'A First Meetup',
+        'location': 'New York',
+        'slug': 'a-first-meetup'
+        },
+        {
+        'title': 'A Second Meetup',
+        'location': 'Paris',
+        'slug': 'a-second-meeetup'
+        }
+        ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'varmeetups': meetups
+        })
