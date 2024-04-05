1bc9d24054171b934fa9fed67157be719646b666
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-05 16:47:04 -0300

    slug changed to number

--- django_project/meetups/views.py
+++ django_project/meetups/views.py
@@ -5,25 +5,32 @@ def index(request):
         {
         'title': 'A First Meetup',
         'location': 'New York',
-        'slug': 'a-first-meetup'
+        'slug': 1
         },
         {
         'title': 'A Second Meetup',
         'location': 'Paris',
-        'slug': 'a-second-meeetup'
+        'slug': 2
         }
         ]
     return render(request, 'meetups/index.html', {
         'show_meetups': True,
-        'varmeetups': meetups
+        'meetups': meetups
         })
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = {
+    selected_meetup = [{
         'title': 'A First Meetup',
         'description': 'This is the first meetup!'
+        },
+        {
+        'title': 'A Second Meetup',
+        'description': 'This is the second meetup!'
         }
+        ]
+    numb=int(meetup_slug)-1
+
     return render(request, 'meetups/meetup-details.html', {
-        'meetup_title': selected_meetup ['title'],
-        'meetup_description': selected_meetup['description']
+        'meetup_title': selected_meetup[numb]['title'],
+        'meetup_description': selected_meetup[numb]['description']
         })
