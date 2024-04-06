98ab49143f269c5b84b89429a86e2ff3e49f878f
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-06 04:55:50 -0300

    feat: Add try and except

--- django_project/meetups/views.py
+++ django_project/meetups/views.py
@@ -1,36 +1,21 @@
 from django.shortcuts import render
+from .models import Meetup
 
 def index(request):
-    meetups = [
-        {
-        'title': 'A First Meetup',
-        'location': 'New York',
-        'slug': 1
-        },
-        {
-        'title': 'A Second Meetup',
-        'location': 'Paris',
-        'slug': 2
-        }
-        ]
+    meetups = Meetup.objects.all()
     return render(request, 'meetups/index.html', {
-        'show_meetups': True,
         'meetups': meetups
         })
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = [{
-        'title': 'A First Meetup',
-        'description': 'This is the first meetup!'
-        },
-        {
-        'title': 'A Second Meetup',
-        'description': 'This is the second meetup!'
-        }
-        ]
-    numb=int(meetup_slug)-1
-
-    return render(request, 'meetups/meetup-details.html', {
-        'meetup_title': selected_meetup[numb]['title'],
-        'meetup_description': selected_meetup[numb]['description']
-        })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup_title': selected_meetup.title,
+            'meetup_description': selected_meetup.description,
+            })
+    except Exception as exc:
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': False
+            })
