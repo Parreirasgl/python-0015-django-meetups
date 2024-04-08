b97f3e03611eba2b94b654a31eaabbe80bb2109c
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-08 08:01:07 -0300

    feat: Create Participant model

--- django_project/meetups/admin.py
+++ django_project/meetups/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Meetup, Location
+from .models import Meetup, Location, Participant
 admin.site.register(Meetup)
 admin.site.register(Location)
+admin.site.register(Participant)
--- django_project/meetups/models.py
+++ django_project/meetups/models.py
@@ -6,12 +6,20 @@ class Location(models.Model):
     address = models. CharField(max_length=300)
     def __str__(self): 
         return f'-- {self.name}'
+    
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return self.email
 
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
+    organizer_email = models.EmailField()
+    date = models.DateField()
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
     def __str__(self): 
         return f'-- {self.title}'
